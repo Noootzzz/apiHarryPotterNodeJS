af9938094475df034e50b2c07d887b8b4fed9ad7
Noot <nathan.boisleux@edu.devinci.fr>
2024-05-03 12:32:24 +0200

    adding lastHouseVisited

--- index.js
+++ index.js
@@ -3,12 +3,29 @@ const routes = require("./routes/start");
 const cors = require("cors");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+
+const ip = require("ip");
+const ipAddr = ip.address();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", routes);
 
+let lastHouseVisited = "";
+
+app.get("/", (req, res) => {
+  res.json({ message: lastHouseVisited });
+});
+
+app.post("/", (req, res) => {
+  lastHouseVisited = req.body.house;
+  console.log("Valeur reçue du client:", lastHouseVisited);
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Serveur running : http://${ipAddr}:3000`);
 });
